99a8241c385ab803fa2b08c028f11c47dede3bb9
Diego <diego.ullua@outlook.com>
2021-01-04 00:59:57 -0300

    agregue comentarios

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Spinner from './components/Spinner'
 
 function App() {
 
+
+  // Estado inicial, fetching para saber cuando esta cargando, y las opciones
   const [state, setState] = useState({
     fetching: false,
     options: [],
@@ -15,40 +17,52 @@ function App() {
 
   const {fetching, options } = state; 
 
+  // Categoria seleccionada para pasar como prop de initial value 
+  // Se recupera la ultima categoria buscada desde local storage 
   const [categoriaSeleccionada , setCategoria] = useState("");
 
+
+  // resultado de la api
   const [resultadoBusqueda, setResultadoBusqueda] = useState([]);
   
 
   useEffect(()  => {
 
+      // si no hay una categoria seleccionada seteamos las categorias desde la API
       if(!categoriaSeleccionada){
  
       const cargarCategorias  = async () => {
 
          setState({...state, fetching: true})
-         const respuesta = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`)
-
-      
+         // consultar la api
+          const respuesta = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`)
+    
           const {drinks} = respuesta.data
+
+          // en el caso de existir, se guarda la ultima busqueda en el state
           const lastSearch = JSON.parse(localStorage.getItem('lastSearch'));
           setCategoria(lastSearch)
+
+          // guardamos las categorias de la respuesta de la api
           let opc = [];
           drinks.map(drink => opc.push(drink.strCategory) );
 
           setState({...state, options: opc, fetching: false})
       }
+
       cargarCategorias();
+
+
       }else {
-        
+
+          // funcion que setea los resultados de la busqueda por esa categoria
           const cargarResultados = async () =>{
          
           const resultado = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoriaSeleccionada}`)
-          console.log('resultado')
-          console.log(resultado.data.drinks)
+          
           setResultadoBusqueda(resultado.data.drinks)
       
-        }
+          }
 
         cargarResultados()
       }
@@ -56,6 +70,8 @@ function App() {
       //eslint-disable-next-line
   }, [categoriaSeleccionada])
 
+
+  // Funcion que deja como unica opcion del select a la categoria seleccionada
   const dejarUnaOpcion = () =>{
 
     if(categoriaSeleccionada){
@@ -67,6 +83,8 @@ function App() {
     
   }
 
+
+  // funcion que borra la busqueda realizada
   const borrarBusqueda = () =>{
     setCategoria("")
     setResultadoBusqueda("")
@@ -78,34 +96,36 @@ function App() {
     <div className="App bg-gradient-danger">
       <div className="container col-6 pt-4 mt-3"> 
       <h1>EJERCICIO DATCOM</h1>
-      {!fetching ? 
-         <div>
-
-          <SelectOpciones
-            opciones={options}
-            initialValue={categoriaSeleccionada}
-            setCategoria={setCategoria} 
-            />
-   
-           <div className="d-flex pt-3 align-items-start">
-   
-             <button
-             className="btn btn-primary mr-2"
-             onClick={()=>setState({...state, options: []})}
-             >Quitar opciones</button>
-             <button
-             className="btn btn-warning mr-2"
-             onClick={dejarUnaOpcion}
-             >Dejar como unica opcion</button>
-   
-             <button
-             className="btn btn-danger"
-             onClick={borrarBusqueda}
-             >
-             Borrar Busqueda </button>
-           </div>
+      {!fetching
+      
+      
+        ? <div>
+              <SelectOpciones
+              opciones={options}
+              initialValue={categoriaSeleccionada}
+              setCategoria={setCategoria} 
+              />
+      
+              <div className="d-flex pt-3 align-items-start">
+      
+                <button
+                className="btn btn-primary mr-2"
+                onClick={()=>setState({...state, options: []})}
+                >Quitar opciones</button>
+                <button
+                className="btn btn-warning mr-2"
+                onClick={dejarUnaOpcion}
+                >Dejar como unica opcion</button>
+      
+                <button
+                className="btn btn-danger"
+                onClick={borrarBusqueda}
+                >
+                Borrar Busqueda </button>
+              </div>
    
          </div>
+
       : <Spinner/> } 
      
 
@@ -114,8 +134,7 @@ function App() {
       
       
       {resultadoBusqueda
-      ? 
-        <div className="mt-4">
+      ? <div className="mt-4">
         <ListOfDrinks drinks={resultadoBusqueda}/>
         </div>
     
@@ -124,5 +143,5 @@ function App() {
   );
 }
 
-export default App;
 
+export default App;
--- src/components/SelectOpciones.js
+++ src/components/SelectOpciones.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 
 
 const SelectOpciones = ({opciones = [], fetching, initialValue, setCategoria}) => {
-   
+    
+    // funcion que setea la caetegoria y guarda el valor en localstorage
     const cambiarCategoria = (e) =>{
-        console.log(e.target.value)
         setCategoria(e.target.value)
         localStorage.setItem("lastSearch",JSON.stringify(e.target.value))
       
         
     }
 
-    
+    // si hay una o ninguna opcion mosramos el select en disabled
     if(opciones.length <= 1)  {
         return <select className="form-control" disabled
             >
@@ -24,7 +24,7 @@ const SelectOpciones = ({opciones = [], fetching, initialValue, setCategoria}) =
     }
 
     
-    
+    // si hay mas de una opcion mostramos el select normalmente
      return( 
         
          <select 
@@ -32,10 +32,11 @@ const SelectOpciones = ({opciones = [], fetching, initialValue, setCategoria}) =
          onChange={cambiarCategoria} 
          >
         <option>Seleccionar</option>
-
-        {opciones.map(opcion => (
+        
+        { // iteracion que marca como selected si existe la prop de initial value en las opciones del select
+        opciones.map(opcion => (
             opcion === initialValue
-
+            
             ? <option value={opcion} key={opcion} selected> {opcion}</option>
 
             : <option value={opcion} key={opcion}> {opcion}</option>     
